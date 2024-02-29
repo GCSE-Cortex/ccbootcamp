b1636198693e7497a5b03722b3964ba39e5ed80e
ruegreen <ruegreen@cisco.com>
2024-02-29 12:42:53 -0700

    Update Set Your Global Vars AllInOne CCTSA Shared Tenant ONLY.js

--- scripty/Set Your Global Vars AllInOne CCTSA Shared Tenant ONLY.js
+++ scripty/Set Your Global Vars AllInOne CCTSA Shared Tenant ONLY.js
@@ -5,26 +5,26 @@ const showEmail = true;
 const showSMS = true;
 const showWhatsApp = true;
 //This is your IMI WIDGET GUID
-const MyChatWidgetGUID = "b2dfd305-ac8c-11ed-89a4-06b1006a3479";
+const MyChatWidgetGUID = "b2dfd305-ac8c-11ed-89a4-06b1006a3247";
 //This is your voice WxCC IVR phone number
 const WXCC_TELEPHONE_NUMBER = "8008667530";
 //This is the URL for starting a SMS conversations with WxConnect
-const IMI_SMS_WEBHOOK = "https://hooks.us.webexconnect.io/events/TBUY8ARMO6";
+const IMI_SMS_WEBHOOK = "https://hooks.us.webexconnect.io/events/TBUY8AR780";
 //This is the URL for requesting and callback, the WxConnect Script runs a HTTP POST to initial a call back request.
-const IMI_CALLBACK_WEBHOOK = "https://hooks.us.webexconnect.io/events/6UOEYABH3W";
+const IMI_CALLBACK_WEBHOOK = "https://hooks.us.webexconnect.io/events/6UOEYAB234";
 //Put your user name, their email address and phone number here, hard coded for JDS events etc.
 //These are used for JDS functions
 const TOKEN_SERVICE_USERID = "YOUR USERID";
-const TOKEN_SERVICE_URL = "https://us-central1-gcp-ruegreen-nprd-41696.cloudfunctions.net/token-service?name=JDS";
-const TOKEN_SERVICE_PASSPHRASE = "mypassword";
-const JDS_PROJECTID = "64b55afb6d82725e4ec46d8b";
+const TOKEN_SERVICE_URL = "https://us-central1-gcp-ruegreen-nprd-41696.cloudfunctions.net/token-service?name=Token_Name";
+const TOKEN_SERVICE_PASSPHRASE = "yourpassword";
+const JDS_PROJECTID = "64b55afb6d82725e4ec46e45";
 //DONT CHANGE THIS!!
 const JDS_POSTURL = "https://api-jds.prod-useast1.ciscowxdap.com/publish/v1/api/event?workspaceId=";
 var JDS_Token = "";
 const GlobalGUID = crypto.randomUUID();
 var UserIdentified = false;
 var UserIdentity = "unknown";
-const demoToolboxUserId = "1883";
+const demoToolboxUserId = "1111";
 //These are images for Agent's face on OffCanvas Menu and success / failure Modals
 const AGENT_IMAGE = "https://wxcc-widgets.s3.us-west-1.amazonaws.com/agent.png";
 const SUCCESS_IMAGE = "https://wxcc-widgets.s3.us-west-1.amazonaws.com/success.svg";
@@ -37,7 +37,7 @@ const WHATSAPP_IMAGE = "https://demowebsite-10871.web.app/images/WhatsAppImage.p
 //Here are the values we used in the CCBOOTCAMP, adjust them for SMS webhook
 const firstName = "Rue";
 const lastName = "Green";   
-const phone = "+13033249089";
+const phone = "+13033243333";
 const email = "ruegreen@cisco.com";
 const balance = "10";
 const myLink = "http://www.google.com";
